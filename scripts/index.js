1c7b7250ace353c7ca075fc80c7fd86dd44ec869
amir guidoum <amirguidoum49@gmail.com>
2024-12-24 23:32:57 -0500

    update

--- scripts/index.js
+++ scripts/index.js
@@ -24,16 +24,48 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
-const profileEditButton = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-modal");
-const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileFormElement = editModal.querySelector("#form");
 
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#name");
+const editModalDescriptionInput = editModal.querySelector("#description");
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal__opened");
+}
+const data = initialCards;
+const cardTemplate = document.querySelector("#card-template");
+const CardsList = document.querySelector(".cards__list");
+function getCardElement(data) {
+  const CardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const CardNameEl = CardElement.querySelector(".card__title");
+  CardNameEl.textContent = data.name;
+  const CardImage = CardElement.querySelector(".card__image");
+  CardImage.src = data.link;
+  CardImage.alt = data.name;
+  return CardElement;
+}
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  CardsList.prepend(cardElement);
 }
 
 function closeModal() {
   editModal.classList.remove("modal__opened");
 }
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  editModal.classList.remove("modal__opened");
+}
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
